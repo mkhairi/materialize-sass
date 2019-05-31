e445a225f552767d2f6d57aab19943c3c654f0f6
Sebastian Schimansky <schimansky@consultiiq.de>
2019-05-31 10:36:40 +0200

    Fixing an issue which caused dropdownlists to flicker
    Chromium bug: #941910

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1771,7 +1771,7 @@ if (Vel) {
             hideDropdown();
             $(document).off('click.' + activates.attr('id'));
           });
-        }, 0);
+        }, 100);
       }
 
       function hideDropdown() {
